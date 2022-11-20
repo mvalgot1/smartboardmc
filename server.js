7adbacf71cb14bb2020c59a7e711485195909cc3
Manichandan Valgot <mvalgot1@student.gsu.edu>
2022-11-20 17:22:36 -0500

    updated server

--- server.js
+++ server.js
@@ -189,7 +189,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 server.listen(8080, () => {
